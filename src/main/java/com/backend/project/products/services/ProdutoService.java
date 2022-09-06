7204671da965f462a777dc0f07f24844fd730b4b
jrodrigues <julio.rodrigues@foursys.com.br>
2022-09-06 11:19:42 -0300

    Adaptando o controller e refatorando código

--- src/main/java/com/backend/project/products/services/ProdutoService.java
+++ src/main/java/com/backend/project/products/services/ProdutoService.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.backend.project.products.model.Produto;
+import com.backend.project.products.model.exception.ResourceNotFoundException;
+import com.backend.project.products.repository.ProdutoRepository;
 import com.backend.project.products.repository.ProdutoRepository_old;
 import com.backend.project.products.shared.ProdutoDTO;
 
@@ -16,39 +18,84 @@ import com.backend.project.products.shared.ProdutoDTO;
 public class ProdutoService {
 
     @Autowired
-    private ProdutoRepository_old produtoRepository;
+    private ProdutoRepository produtoRepository;
 
     // Obter todos 
     public List<ProdutoDTO> obterTodos(){
 
-        List<Produto> produtos = produtoRepository.obterTodos();
+        List<Produto> produtos = produtoRepository.findAll();
+
         return produtos.stream().map(produto -> new ModelMapper().map(produto, ProdutoDTO.class))
         .collect(Collectors.toList());
     }
 
     // Obter produto por ID
     public Optional<ProdutoDTO> obterPorId(Integer id){
-         return produtoRepository.obterPorId(id);
+
+         Optional<Produto> produto = produtoRepository.findById(id);
+
+         // Se não encontrar o produto
+         if(produto.isEmpty()){
+            throw new ResourceNotFoundException("Produto com id" + id + "não encontrado");
+         }
+
+         // Transformando em produto DTO
+         ProdutoDTO dto = new ModelMapper().map(produto.get(), ProdutoDTO.class);
+         return Optional.of(dto); 
     }
 
     // Adicionar o produto na lista de produtos
-    public ProdutoDTO adicionar(ProdutoDTO produto){
-        return produtoRepository.adicionar(produto);
+    public ProdutoDTO adicionar(ProdutoDTO produtoDto){
+
+        // Removendo ID para cadastrar o produto no banco 
+        produtoDto.setId(null);
+
+        // Criando objeto de mapeamento
+        ModelMapper mapper = new ModelMapper();
+
+        // Converter para Produto
+        Produto produto = mapper.map(produtoDto, Produto.class);
+
+        // Salvar o produto no banco
+        produto = produtoRepository.save(produto);
+
+        // Retornar ProdutoDTO atualizado
+        produtoDto.setId(produto.getId());
+
+        return produtoDto;
     }
 
     // Deletando produto da lista 
     public void deletar(Integer id){
-         produtoRepository.deletar(id);
+
+        // Verificar se o produto existe 
+        Optional<Produto> produto = produtoRepository.findById(id);
+
+        if(produto.isEmpty()){
+            throw new ResourceNotFoundException("Não foi possível deletar o produto do id" + id );
+        }
+        
+        // Deletar o produto pelo ID
+         produtoRepository.deleteById(id);
     }
 
     // Atualizar produto da lista
-    public ProdutoDTO atualizar(Integer id, ProdutoDTO produto){
+    public ProdutoDTO atualizar(Integer id, ProdutoDTO produtoDto){
 
-        // Fazer alguma validação no ID
-        produto.setId(id);
+        // Passar o Id para o produtoDto
+        produtoDto.setId(id);
+
+        // Criar um objeto de mapeamento 
+        ModelMapper mapper = new ModelMapper();
+
+        // Converter o DTO para Produto
+        Produto produto = mapper.map(produtoDto, Produto.class);
+
+        // Atualizar o produto no banco de dados
+        produtoRepository.save(produto);
         
-        // Atualizar produto
-        return produtoRepository.atualizar(produto);
+        // Retornar o produtoDTO atualizado
+        return produtoDto;
     }
     
 }
--- src/main/java/com/backend/project/products/view/controller/ProdutoController.java
+++ src/main/java/com/backend/project/products/view/controller/ProdutoController.java
@@ -2,8 +2,13 @@ package com.backend.project.products.view.controller;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+import org.apache.catalina.connector.Response;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +21,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.backend.project.products.model.Produto;
 import com.backend.project.products.services.ProdutoService;
+import com.backend.project.products.shared.ProdutoDTO;
+import com.backend.project.products.view.model.ProdutoRequest;
+import com.backend.project.products.view.model.ProdutoResponse;
 
 @RestController
 @RequestMapping("/api/produtos")
@@ -25,29 +33,70 @@ public class ProdutoController {
     private ProdutoService produtoService;
 
     @GetMapping
-    public List<Produto> obterTodos(){
-        return produtoService.obterTodos();
+    public ResponseEntity<List<ProdutoResponse>> obterTodos(){
+
+        List<ProdutoDTO> produtos =  produtoService.obterTodos();
+
+        ModelMapper mapper = new ModelMapper();
+
+        List<ProdutoResponse> resposta = produtos.stream().map(produtoDto -> mapper.map(produtoDto, ProdutoResponse.class))
+        .collect(Collectors.toList());
+
+        return new ResponseEntity<>(resposta, HttpStatus.OK);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<ProdutoResponse>> obterPorId(@PathVariable Integer id){
+
+        try {
+            Optional<ProdutoDTO> dto =  produtoService.obterPorId(id);
+
+            // Converter para ProdutoResponseEntity 
+            ProdutoResponse produto = new ModelMapper().map(dto.get(), ProdutoResponse.class);
+
+            // Retorna 
+            return new ResponseEntity<>(Optional.of(produto), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
     }
 
     @PostMapping
-    public Produto adicionar(@RequestBody Produto produto){
-        return produtoService.adicionar(produto);
-    }
+    public ResponseEntity<ProdutoResponse> adicionar(@RequestBody ProdutoRequest produtoReq){
 
-    @GetMapping("/{id}")
-    public Optional<Produto> obterPorId(@PathVariable Integer id){
-        return produtoService.obterPorId(id);
+        // Converter para produtoDTO
+        ModelMapper mapper = new ModelMapper();
 
+        ProdutoDTO produtoDto = mapper.map(produtoReq, ProdutoDTO.class);
+
+        produtoDto = produtoService.adicionar(produtoDto);
+
+        //  Retornar um ProdutoResponse
+        return new ResponseEntity<>(mapper.map(produtoDto, ProdutoResponse.class), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
-    public void deletar(@PathVariable Integer id){
+    public ResponseEntity<?> deletar(@PathVariable Integer id){
         produtoService.deletar(id);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @PutMapping("/{id}")
-    public Produto atualizar(@RequestBody Produto produto, @PathVariable Integer id){
-        return produtoService.atualizar(id, produto);
+    public ResponseEntity<ProdutoResponse> atualizar(@RequestBody ProdutoRequest produtoReq, @PathVariable Integer id){
+
+        ModelMapper mapper = new ModelMapper();
+
+        ProdutoDTO produtoDto = mapper.map(produtoReq, ProdutoDTO.class);
+
+        produtoDto = produtoService.atualizar(id, produtoDto);
+
+        return new ResponseEntity<>(
+            mapper.map(produtoDto, ProdutoResponse.class),
+            HttpStatus.OK
+        );
     }
 
 
